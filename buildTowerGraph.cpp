11cd74031dea10822dfda6ba551aa3c2419d853a
David Hallac <david.hallac@gmail.com>
2014-07-17 14:18:47 -0700

    Added edge weights

--- buildTowerGraph.cpp
+++ buildTowerGraph.cpp
@@ -174,8 +174,8 @@ int main(int argc, const char * argv[])
 		var1 = var1/(height - 1);
 		var2 = var2/(height - 1);
 		sampleCov = sampleCov / (height - 1);
-		//cout << var1 << ", " << var2 << ", " << sampleCov << ", " << mean1 << ", " << mean2 << ", " << id1 << ", " << id2 << "\n";
-		sampleCov = sampleCov / (var1*var2);
+		cout << var1 << ", " << var2 << ", " << sampleCov << ", " << mean1 << ", " << mean2 << ", " << id1 << ", " << id2 << "\n";
+		sampleCov = sampleCov / (var1*var2+0.001);
 
 		TFlt weight = max(sampleCov, TFlt(0));
 

1896e688838cff59d5a654a3e6075f00e1d77160
David Hallac <david.hallac@gmail.com>
2014-07-17 20:37:35 -0700

    added edge weights

--- buildTowerGraph.cpp
+++ buildTowerGraph.cpp
@@ -194,7 +194,7 @@ int main(int argc, const char * argv[])
 			temp.Add(NI.GetId());
 			temp.Add(NI.GetOutNId(0));
 			TFlt weight = edgeWeights.GetDat(temp); //TODO: SOLVE THIS
-			if(int(weight) == 0)
+			if(double(weight) <= 0.00001)
 			{
 				G->DelEdge(NI.GetId(), NI.GetOutNId(0) );
 			}
